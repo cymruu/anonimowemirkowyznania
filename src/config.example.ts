9fd117f6fda4ca16c62c019db7ec0cc0cb9911b8
Filip Bachul <filipbachul@gmail.com>
2020-04-15 18:33:14 +0200

    added privacy notice on index, added info to config example

--- src/config.example.ts
+++ src/config.example.ts
@@ -9,7 +9,7 @@ export default {
 		username: 'username', //username
 		password: 'password', //password and username are required to handle surverys
 	},
-	siteURL: 'https://localhost:1337', //site url
+	siteURL: 'https://localhost:1337', //site url CAN NOT HAVE / at end
 	mongoURL: 'mongodb://username:password@host/database_name',
 	secret: 'secret_for_signing_jwt', //website's secret key,
 	websocketPort: 8090, //port on which a websockets server will work
