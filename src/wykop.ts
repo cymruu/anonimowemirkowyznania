ecb3ceafe257730a257d9e4bcdabb9d0c256db26
Filip Bachul <filipbachul@gmail.com>
2020-07-18 19:09:29 +0200

    added log interceptor

--- src/wykop.ts
+++ src/wykop.ts
@@ -1,6 +1,14 @@
 import config from './config'
 import { Wykop, Client, CreateWykopService } from 'wypokjs'
+import { WykopError } from 'wypokjs/dist/wykop'
+import { AxiosError } from 'axios'
+import logger from './logger'
 
-export const wykop = new Wykop(config.wykopConfig)
+const logInterceptor = (err: WykopError | AxiosError) => {
+	logger.error(err.toString())
+	return err
+}
+
+export const wykop = new Wykop(config.wykopConfig, [logInterceptor])
 export const client = new Client(wykop, config.wykopClientConfig)
 export const service = CreateWykopService(client)
