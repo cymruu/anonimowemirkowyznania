02ce4d86c892e8d35f199cf7557dd72bb74b0a52
Filip Bachul <filipbachul@gmail.com>
2020-07-18 13:43:57 +0200

    updated wypokjs library

--- src/wykop.ts
+++ src/wykop.ts
@@ -4,4 +4,3 @@ import { Wykop, Client, CreateWykopService } from 'wypokjs'
 export const wykop = new Wykop(config.wykopConfig)
 export const client = new Client(wykop, config.wykopClientConfig)
 export const service = CreateWykopService(client)
-
