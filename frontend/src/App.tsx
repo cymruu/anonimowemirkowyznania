f6914acb3ca12f0a0d68fda687b751b68e857ac4
Filip Bachul <filipbachul@gmail.com>
2020-09-01 17:48:43 +0200

    working on react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+import { Link as RouterLink, Router, RouteComponentProps } from '@reach/router'
+import { Index } from './Index';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { AppBar, Toolbar, IconButton, Typography, Button, makeStyles, Link } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Anonimowe Mirko Wyznania
+          </Typography>
+          <Button color="inherit">
+            <Link component={RouterLink} to="/login" color="inherit">Login</Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Router>
+        <Index path="/" />
+        {/* <Login path="/login" /> */}
+      </Router>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ frontend/src/Index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+
+export function Index(props: RouteComponentProps){
+    return(
+        <div>
+            index
+        </div>
+    )
+}
