e2af62491e78508cf8dc01dd640aeb6cff02a636
Filip Bachul <filipbachul@gmail.com>
2020-09-01 18:47:45 +0200

    working on login form

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { RouteComponentProps } from "@reach/router";
 import { TextField, Container, makeStyles, Button } from "@material-ui/core";
 
@@ -13,24 +13,42 @@ const useStyles = makeStyles((theme) => ({
 
 export function Login(props: RouteComponentProps) {
     const classes = useStyles()
+    const [login, setLogin] = useState("")
+    const [password, setPassword] = useState("")
 
+    useEffect(() => {
+        async function loginUser() {
+            const response = await fetch('api/login', { method: 'post', body: JSON.stringify({ login, password }) })
+            const json = await response.json()
+            console.log(json);
+        }
+        loginUser()
+    }, [login, password])
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+    }
     return (
         <Container maxWidth="xs">
-            <form noValidate autoComplete="off" className={classes.form}>
+            <form noValidate autoComplete="off" className={classes.form} onSubmit={handleSubmit}>
                 <div>
                     <TextField label="Username" variant="outlined"
                         margin="normal"
                         required
                         fullWidth
                         name="login"
+                        value={login}
+                        onChange={e => setLogin(e.target.value)}
                     />
                 </div>
                 <div>
-                <TextField label="Password" variant="outlined"
+                    <TextField label="Password" variant="outlined"
                         margin="normal"
                         required
                         fullWidth
                         name="login"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
                         type="password"
                     />
                 </div>
