1a6098c484dcb395edf6a7b826eb4163d2d58dad
Filip Bachul <filipbachul@gmail.com>
2020-09-03 22:36:37 +0200

    action buttons

--- frontend/src/components/ActionButtons.tsx
+++ frontend/src/components/ActionButtons.tsx
@@ -1,12 +1,29 @@
-import { Button, CircularProgress } from "@material-ui/core";
+import { Button, CircularProgress, Grid } from "@material-ui/core";
 import { RouteComponentProps } from "@reach/router";
 import React, { Fragment, useState } from "react";
 import { SuccessButton } from "./SuccessButton";
 
 interface ActionButtonsProps {
-    confession: object
-    acceptFn: (confession: object, event: any) => Promise<any>
+    confession: any
+    acceptFn: (confession: any, event: any) => Promise<any>
     declineFn?: () => void
+}
+
+const getRedButtonProps = (confession: any) => {
+    switch (confession.status) {
+        case -1:
+            return {
+                text: 'Undecline'
+            }
+        case 0:
+            return {
+                text: 'Decline'
+            }
+        case 1:
+            return {
+                text: 'Remove'
+            }
+    }
 }
 
 export function ActionButtons(props: RouteComponentProps & ActionButtonsProps) {
@@ -14,22 +31,25 @@ export function ActionButtons(props: RouteComponentProps & ActionButtonsProps) {
 
     const { acceptFn, declineFn, confession } = props
 
-    const acceptWrapper = (confession: object, event: any) => {
+    const actionWrapper = (actionFn: (...args: any) => Promise<any>) => (confession: object, event: any) => {
         setSending(true)
-        acceptFn(confession, event)
+        actionFn(confession, event)
             .then().finally(() => {
                 setSending(false)
             })
     }
 
+    const button = getRedButtonProps(confession)
     return (
         <Fragment>
-            <SuccessButton disabled={isSending} variant="contained" onClick={e => acceptWrapper(confession, (e as any))}>
-                {isSending ? <CircularProgress size={24} /> : 'Accept'}
-            </SuccessButton>
-            <Button disabled={isSending} variant="contained" color="secondary">
-                Decline
-            </Button>
+            <Grid container direction="column">
+                <SuccessButton disabled={isSending || confession.status === 1} variant="contained" onClick={e => actionWrapper(acceptFn)(confession, e)}>
+                    {isSending ? <CircularProgress size={24} /> : 'Accept'}
+                </SuccessButton>
+                <Button disabled={isSending} variant="contained" color="secondary">
+                    {isSending ? <CircularProgress size={24} /> : button?.text}
+                </Button>
+            </Grid>
         </Fragment>
     )
 }
