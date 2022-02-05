858dd77f9110a2b43761042c0f4e0f861df08880
Filip Bachul <filipbachul@gmail.com>
2022-02-05 12:23:08 +0100

    remove unnecessary success button component

--- frontend/src/components/SuccessButton.tsx
+++
@@ -1,9 +0,0 @@
-
-import { Button, Theme } from '@mui/material';
-import { styled } from '@mui/styles';
-
-export default styled(Button)<Theme>(({ theme }) => ({
-  backgroundColor: theme.palette.success.main,
-  color: theme.palette.common.white,
-  marginBottom: 5,
-}))
